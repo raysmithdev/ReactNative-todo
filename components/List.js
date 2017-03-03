752bfc252b2de315db7c2884a37be8f4919c9ef7
CodeforLiberty <sswell@live.com>
2017-03-03 12:20:36 -0500

    Started on Android

---
+++ components/List.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Container, Content, ListItem, Text, Separator } from 'native-base';
+
+
+const List = ({ todos, deleteToDo }) => (
+  
+);
+
+export default List;
+ };
---
+++ index.android.js
@@ -0,0 +1,97 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View, 
+  TextInput, 
+} from 'react-native';
+
+import {Button, Form, Input } from 'native-base'; 
+
+
+export default class Main extends Component {
+  constructor() {
+    super()
+
+    this.counter = 0;
+
+    this.state = {
+      todos: []
+    }
+
+    this.addToDo = this.addToDo.bind(this);
+    this.deleteToDo = this.deleteToDo.bind(this);
+  }
+
+  addToDo(text) {
+    
+  
+  
+
+    this.setState({ todos:text });
+  }
+
+deleteToDo(todoId) {
+    const todos = this.state.todos.filter(todo => todo.id !== todoId);
+
+    this.setState({ todos });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          TO DO:
+        </Text>
+        <Text style={styles.instructions}>
+          To get started tap on the form and enter a thing that needs done
+        </Text>
+      
+        <Text style={styles.instructions}>
+         {this.state.todos};
+        </Text>
+        <Form>
+        <Input placeholder="Get groceries" id="task"/>
+        <Button onPress={ this.addToDo}>
+          <Text>Click Me! </Text>
+        </Button>
+        </Form>
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 10,
+    color:'green'
+  },
+  instructions: {
+    color: 'grey',
+    marginBottom: 5,
+    fontSize: 20,
+  },
+  input: {
+    width:150,
+    color:'blue',
+
+  }
+});
+
+AppRegistry.registerComponent('Main', () => Main);
